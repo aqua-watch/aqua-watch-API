34af28cfd9377d5142eab845591992b52dd5255f
Stephanie Velez <stephanievelez@dhcp-wifi-8021x-155-41-22-108.bu.edu>
2018-02-28 18:44:55 -0500

    Skeleton of front-end

--- aqua-watch-api/aqua-watch-api/app.py
+++ aqua-watch-api/aqua-watch-api/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, request, redirect, session, flash
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-    
+def index():
+    return render_template('index.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+@app.route('/your_water_quality')
+def your_water_quality():
+    return render_template('your_water_quality.html')
+
+@app.route('/report_guide')
+def report_guide():
+    return render_template('report_guide.html')
---
+++ aqua-watch-api/aqua-watch-api/static/js/script.js
@@ -0,0 +1,29 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+/* Google Maps */
+var map;
+function initMap() {
+
+	var myLatLng = new google.maps.LatLng(42.349341, -71.103982);
+	var mapOptions = {
+		zoom: 15,
+		center: myLatLng
+	}
+	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		title: 'Marker'
+	})
+}
