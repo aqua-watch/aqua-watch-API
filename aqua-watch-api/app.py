4ee17078cbe56073d8bd4d239a945ca37b8335fd
svelez17 <svelez17@bu.edu>
2018-04-07 17:00:14 -0400

    Added Google OAuth 2.0

--- aqua-watch-api/app.py
+++ aqua-watch-api/app.py
@@ -1,17 +1,35 @@
 ###########################################
-## AquaWatch
 ##
+## AquaWatch
 ##
 ###########################################
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
+import flask
+import os
+import requests
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
 from flask_googlemaps import GoogleMaps, Map
 from pymongo import MongoClient
 from bson import json_util
-import urllib.parse, urllib.request, json, pprint
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 from ast import literal_eval
-import requests
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+
+# This variable specifies the name of a file that contains the OAuth 2.0
+# information for this application, including its client_id and client_secret.
+CLIENT_SECRETS_FILE = "client_secret.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['profile','email']
+API_SERVICE_NAME = 'plus'
+API_VERSION = 'v1'
 
 # initialize Flask
 app = Flask(__name__)
@@ -19,9 +37,13 @@ app = Flask(__name__)
 # connect to Google Maps API
 GoogleMaps(app, key='AIzaSyDC7vvV9T9pQBTeXIc-edWfP20tPO-dx0A')
 
+
+"""
 # begin code used for login
 login_manager = LoginManager()
 login_manager.init_app(app)
+"""
+
 app.config.update(
     SECRET_KEY = 'secret_xxx'
 )
@@ -40,35 +62,271 @@ users = db.users
 # the user
 user = None
 
+
+"""
+class User():
+
+    def __init__(self, email, first_name, last_name, displayName):
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.displayName = displayName
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False 
+
+    def get_id(self):
+        return self.email
+"""
+
+"""
 class User(UserMixin):
-    """ User class that represents an arbitrary user
-    """
+    # User class that represents an arbitrary user
     first_name = None
     last_name = None
+    email = None
 
     def __init__(self, email):
-        """ initialize class
-            :param id: email
-        """
-        self.id = email
+        # initialize class
+        #    :param id: email
+
+        self.email = email
+
 
 @login_manager.user_loader
 def user_loader(email):
-    """ to load user
-    """
+    # to load user
     user = User(email)
     return user
+"""
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def index():
-    """ main page
+    # main page
     """
     message = "Insert address above to see water quality"
     if (user):
         return render_template('index.html', message=message, fname=user.first_name, logged_in=1)
     else:
         return render_template('index.html', message=message, not_logged_in=1)
+    """
+    message = "Insert address above to see water quality"
 
+    if 'credentials' in flask.session:
+
+        # Temporary fix for user_firstName not found error, but should be fixed with database
+        # Load credentials from the session.
+        credentials = google.oauth2.credentials.Credentials(
+            **flask.session['credentials'])
+
+        SERVICE = googleapiclient.discovery.build(
+            API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+        user_resource = SERVICE.people()
+        user_document = user_resource.get(userId='me').execute()
+        user_firstName = user_document['name']['givenName']
+        return render_template('index.html', message=message, logged_in=1, fname=user_firstName)
+    else:
+        return render_template('index.html', message=message, not_logged_in=1)
+
+
+
+# User login with Google account
+@app.route('/login', methods=['GET'])
+def login():
+    if 'credentials' not in flask.session:
+        return flask.redirect('authorize')
+
+    # Load credentials from the session.
+    credentials = google.oauth2.credentials.Credentials(
+        **flask.session['credentials'])
+
+    SERVICE = googleapiclient.discovery.build(
+        API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+    user_resource = SERVICE.people()
+    user_document = user_resource.get(userId='me').execute()
+
+
+    user_displayName = user_document['displayName']
+    user_lastName = user_document['name']['familyName']
+    user_firstName = user_document['name']['givenName']
+    user_email = user_document['emails'][0]['value']
+    user_image = user_document['image']['url']
+
+
+    print user_displayName
+    print user_firstName
+    print user_lastName
+    print user_email
+    print user_image
+
+    # Save credentials back to session in case access token was refreshed.
+    # ACTION ITEM: In a production app, you likely want to save these
+    #              credentials in a persistent database instead.
+    flask.session['credentials'] = credentials_to_dict(credentials)
+
+    message = "Insert address above to see water quality"
+
+    return render_template('index.html', message=message, logged_in=1, fname=user_firstName)
+
+
+
+# Getting authorization from user through Google
+@app.route('/authorize')
+def authorize():
+    # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES)
+
+    flow.redirect_uri = flask.url_for('gCallback', _external=True)
+
+    authorization_url, state = flow.authorization_url(
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true',
+        prompt='select_account')
+
+    # Store the state so the callback can verify the auth server response.
+    flask.session['state'] = state
+
+
+    return flask.redirect(authorization_url)
+
+
+
+@app.route('/gCallback')
+def gCallback():
+    # Specify the state when creating the flow in the callback so that it can
+    # verified in the authorization server response.
+    state = flask.session['state']
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+    flow.redirect_uri = flask.url_for('gCallback', _external=True)
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = flask.request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    # Store credentials in the session.
+    # ACTION ITEM: In a production app, you likely want to save these
+    #              credentials in a persistent database instead.
+    credentials = flow.credentials
+    flask.session['credentials'] = credentials_to_dict(credentials)
+
+    return flask.redirect(flask.url_for('login'))
+
+
+
+# Logout from app, but remained logged in to Google
+# Orginally '/clear' and def clear_credentials()
+@app.route('/logout')
+def logout():
+    if 'credentials' in flask.session:
+        del flask.session['credentials']
+    message = "Insert address above to see water quality"
+
+    return render_template('index.html', message=message, not_logged_in=1)
+
+def credentials_to_dict(credentials):
+  return {'token': credentials.token,
+          'refresh_token': credentials.refresh_token,
+          'token_uri': credentials.token_uri,
+          'client_id': credentials.client_id,
+          'client_secret': credentials.client_secret,
+          'scopes': credentials.scopes}
+
+
+
+
+# This will disconnect a user's Google account from the app
+@app.route('/revoke')
+def revoke():
+  if 'credentials' not in flask.session:
+    return ('You need to <a href="/authorize">authorize</a> before ' +
+            'testing the code to revoke credentials.')
+
+  credentials = google.oauth2.credentials.Credentials(
+    **flask.session['credentials'])
+
+  revoke = requests.post('https://accounts.google.com/o/oauth2/revoke',
+      params={'token': credentials.token},
+      headers = {'content-type': 'application/x-www-form-urlencoded'})
+
+  status_code = getattr(revoke, 'status_code')
+  if status_code == 200:
+    return ('Credentials successfully revoked')
+  else:
+    return('An error occurred.')
+
+"""
+@app.route('/login_attempt', methods=['POST'])
+def login_attempt():
+    # To handle what happens after user provides email and password
+    global user
+    email = str(request.form.get('email'))
+    password = str(request.form.get('password'))
+    cursor = list(users.find({"email": email}))
+    if (len(cursor) == 0):
+        return render_template('login.html', message="You have to register first")
+    elif (cursor[0]["password"] != password):
+        return render_template('login.html', message="Wrong password")
+    else: # cursor[0]["password"] == password
+        message = "Insert address above to see water quality"
+        fname = cursor[0]["first name"]
+        lname = cursor[0]["last name"]
+        user = user_loader(email)
+        user.first_name = fname
+        user.last_name = lname
+        login_user(user)
+        return render_template('index.html', message=message, fname=fname, logged_in=1)
+
+@app.route('/register', methods=['GET'])
+def register():
+    # For the user to provide personal data
+    return render_template('register.html')
+
+@app.route('/register_attempt', methods=['POST'])
+def register_attempt():
+    # To handle what happens whe user provides personal data
+    email = str(request.form.get('email'))
+    password = str(request.form.get('password'))
+    firstname = str(request.form.get('firstname'))
+    lastname = str(request.form.get('lastname'))
+    cursor = list(users.find({"email": email}))
+    if (len(cursor) == 0):
+        dicts = [{"email": email, "password": password, "first name": firstname, "last name": lastname}]
+        docs = json.dumps(dicts)
+        docs = json_util.loads(docs)
+        users.insert_many(docs)
+        return render_template('register.html', message="Registration successful!")
+    else:
+        return render_template('register.html', message="Already registered!")
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    # When action requires login
+    return render_template('login.html')
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    # Log out user and update user global
+    global user
+    user = None
+    logout_user()
+    message = "Insert address above to see water quality"
+    return render_template('index.html', message=message, not_logged_in=1)
+"""
 @app.route('/mapview', methods=['POST'])
 def mapview():
     """ View function to display results from an address in the database
@@ -121,74 +379,6 @@ def mapview():
         else:
             return render_template('index.html', message=message, not_logged_in=1)
 
-@app.route('/login', methods=['GET'])
-def login():
-    """ For user to provide email and password
-    """
-    return render_template('login.html')
-
-@app.route('/login_attempt', methods=['POST'])
-def login_attempt():
-    """ To handle what happens after user provides email and password
-    """
-    global user
-    email = str(request.form.get('email'))
-    password = str(request.form.get('password'))
-    cursor = list(users.find({"email": email}))
-    if (len(cursor) == 0):
-        return render_template('login.html', message="You have to register first")
-    elif (cursor[0]["password"] != password):
-        return render_template('login.html', message="Wrong password")
-    else: # cursor[0]["password"] == password
-        message = "Insert address above to see water quality"
-        fname = cursor[0]["first name"]
-        lname = cursor[0]["last name"]
-        user = user_loader(email)
-        user.first_name = fname
-        user.last_name = lname
-        login_user(user)
-        return render_template('index.html', message=message, fname=fname, logged_in=1)
-
-@app.route('/register', methods=['GET'])
-def register():
-    """ For the user to provide personal data
-    """
-    return render_template('register.html')
-
-@app.route('/register_attempt', methods=['POST'])
-def register_attempt():
-    """ To handle what happens whe user provides personal data
-    """
-    email = str(request.form.get('email'))
-    password = str(request.form.get('password'))
-    firstname = str(request.form.get('firstname'))
-    lastname = str(request.form.get('lastname'))
-    cursor = list(users.find({"email": email}))
-    if (len(cursor) == 0):
-        dicts = [{"email": email, "password": password, "first name": firstname, "last name": lastname}]
-        docs = json.dumps(dicts)
-        docs = json_util.loads(docs)
-        users.insert_many(docs)
-        return render_template('register.html', message="Registration successful!")
-    else:
-        return render_template('register.html', message="Already registered!")
-
-@login_manager.unauthorized_handler
-def unauthorized_handler():
-    """ When action requires login
-    """
-    return render_template('login.html')
-
-@app.route('/logout', methods=['GET'])
-@login_required
-def logout():
-    """ Log out user and update user global
-    """
-    global user
-    user = None
-    logout_user()
-    message = "Insert address above to see water quality"
-    return render_template('index.html', message=message, not_logged_in=1)
 
 @app.route('/map', methods=['GET'])
 def map():
@@ -244,6 +434,9 @@ def report_guide():
         return render_template('report_guide.html', not_logged_in=1)
 
 if __name__ == "__main__":
-    """ to run Flask locally
-    """
+    # When running locally, disable OAuthlib's HTTPs verification.
+    # ACTION ITEM for developers:
+    #     When running in production *do not* leave this option enabled.
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    # To run Flask locally
     app.run(port=5000, debug=True)
